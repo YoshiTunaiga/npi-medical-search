1e599d7104a31acd04fb7d870663b40fa80d648b
Gi Diaz <85481752+YoshiTunaiga@users.noreply.github.com>
2024-07-27 22:32:35 -0400

    update: route and vite config base

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,21 +2,9 @@ import Home from "./components/Home/Home";
 import { useRoutes } from "react-router-dom";
 import NPPage from "./components/NPPage/NPPage";
 
-// import axios from "axios";
-
 function App() {
-  // 1851926703 https://npi-db.org
-  // const fetchAPI = async () => {
-  //   // const response = await axios.get(`https://npi-db.org/api/${1851926703}`);
-  //   const response = await axios.get(`http://localhost:8080/api/${1851926703}`);
-  //   console.log(response.data);
-  // };
-
-  // useEffect(() => {
-  //   fetchAPI();
-  // }, []);
   const routes = useRoutes([
-    { path: "/", element: <Home /> },
+    { path: "/npi-medical-search", element: <Home /> },
     { path: "/api/:npId", element: <NPPage /> },
   ]);
 
